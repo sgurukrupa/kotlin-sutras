a8442c9c84e166e3e3e9e5ff06fc7bcfe4cea65e
Ganesh A Hegde <sgurukrupa@gmail.com>
2020-08-07 21:03:28 +0530

    Added description of integer types, Haribol.

---
+++ sutras.kt
@@ -0,0 +1,26 @@
+/* Haribol!
+ *
+ * Sutra - 1
+ * ---------
+ *
+ * Everything is an object; we can call member functions and properties on any variable.
+ * "Some of the types can have special internal representation - for example,
+ * numbers, characters and booleans can be represented as primitive values at runtime - 
+ * but to the user they look like ordinary classes."
+ *
+ * Sutra - 2
+ * ---------
+ *
+ * Four types of integers:
+ *
+ *    Type    Size (bits)      Range
+ * ---------------------------------------
+ * 1. Byte -   8          [-128, 127]
+ * 2. Short - 16        [-32768, 32767]
+ * 3. Int -   32        [-2e31, 2e31 - 1]
+ * 4. Long -  64        [-2e63, 2e63 - 1]
+ *
+ * Sutra - 3
+ * ---------
+ *
+ */
