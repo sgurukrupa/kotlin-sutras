1e70d3abee1ea9e0064ed9b5bb677b9034f66d71
Ganesh A Hegde <sgurukrupa@gmail.com>
2020-08-07 22:21:45 +0530

    Moved content to sutras.kt, Haribol.

--- sutra-2.kt
+++
@@ -1,9 +0,0 @@
-/* Haribol!
- * Four types of integers
- *    Type    Size (bits)      Range
- * ---------------------------------------
- * 1. Byte -   8          [-128, 127]
- * 2. Short - 16        [-32768, 32767]
- * 3. Int -   32        [-2e31, 2e31 - 1]
- * 4. Long -  64        [-2e63, 2e63 - 1]
- */
