73a8783e07e75370caf98cdaadd2a54a6606579d
Ganesh A Hegde <sgurukrupa@gmail.com>
2020-08-07 22:21:31 +0530

    Moved content to sutras.kt, Haribol.

--- sutra-1.kt
+++
@@ -1,6 +0,0 @@
-/* Haribol!
- * Everything is an object; we can call member functions and properties on any variable.
- * "Some of the types can have special internal representation - for example,
- * numbers, characters and booleans can be represented as primitive values at runtime - 
- * but to the user they look like ordinary classes."
- */
