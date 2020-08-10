e65756bf8cfbfc77a41fbcd803bdb2577cea8d86
Ganesh A Hegde <sgurukrupa@gmail.com>
2020-08-10 22:19:39 +0530

    Variable definition syntax, Haribol!

--- sutras.kt
+++ sutras.kt
@@ -1,6 +1,34 @@
 /* Haribol!
  *
- * #1 - Everything is an object
+ * Sutra #1 - Program execution entry point
+ * The main function is an entry point of a Kotlin app.
+ */
+
+fun main() {
+  println("Hello Guys")
+}
+ 
+
+/* Sutra #2 - Read-only variable definitions
+ * -----------------------------------------
+ *
+ * Read-only variables are defined using val; they can be assigned only once.
+ * Types can be inferred when they are assigned with a value immediately at the point of definition.
+ */
+val a: Int = 1 // explicit type specification isn't required
+val b = 2 // Type is inferred as Int
+val c: Int
+c = 3 // deferred asignment
+
+
+/* Sutra #3 - Read-write variable definitions
+ * ------------------------------------------
+ *
+ * Variables defined using var keyword can be assigned multiple types.
+ */
+
+/*
+ * Sutra #4 - Everything is an object
  * ----------------------------
  *
  * We can call member functions and properties on any variable.
@@ -9,7 +37,7 @@
  * but to the user they look like ordinary classes."
  *
  *
- * #2 - Integer Types
+ * Sutra #5 - Integer Types
  * ------------------
  *
  * Four types of integers:
@@ -22,14 +50,15 @@
  * 4. Long -  64        [-2e63, 2e63 - 1]
  *
  *
- * #3 - Int Type Inference
+ * Sutra #6 - Int Type Inference
  * -----------------------
- * Variables which are initialized with integers not exceeding the maximum value of Int are inferred to be of Int type.
+ * Integers not exceeding the maximum value of Int are inferred to be of type Int.
  */
 val one = 1 // Int
  
  
-/* #4 - Long Type Inference
+/*
+ * Sutra #7 - Long Type Inference
  * ------------------------
  * Variables initialized with values bigger than the maximum value of Int are inferred to be of type Long.
  */
@@ -37,8 +66,29 @@ val big10 = 10000000000 // Long - 10 billion
 val oneLong = 1L // use 'L' suffix to force inferred type as Long
 
 
-/* #5 - No type inferrence for smaller integer types
+/*
+ * Sutra #8 - No type inferrence for smaller integer types
  * -------------------------------------------------
- * Types smaller than Int cannot be inferred; type needs to be specified explicitly
+ * Types smaller than Int cannot be inferred; type needs to be specified explicitly.
  */
 val oneByte: Byte = 1
+
+
+/* 
+ * Sutra #9 - Floating-Point types
+ * -------------------------------
+ * These types are represented and manipulated according to IEEE 754 standard.
+ * There's a type each for representing single and double precision floating-point numbers.
+ *
+ *    Type    Size (bits)  Significant (bits)  Exponent (bits)  Decimal digits
+ * ---------------------------------------------------------------------------
+ * 1. Float   32           24                  8                6-7
+ * 2. Double  64           53                  11               15-16
+ */
+
+/* 
+ * Sutra #10 - Type inference for Double type
+ * -----------------------------------------
+ * Variables initialized with floating-point numbers are inferred to be of type Double.
+ */
+
